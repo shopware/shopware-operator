eb2a9eef699d75d8d8c081aa7c0334d6197e0942
Patrick Derks <patrick.derks@proton.me>
2025-02-17 10:20:34 +0100

    test: add label test

---
+++ internal/util/labels_test.go
@@ -0,0 +1,38 @@
+package util_test
+
+import (
+	"fmt"
+	"testing"
+
+	v1 "github.com/shopware/shopware-operator/api/v1"
+	"github.com/shopware/shopware-operator/internal/util"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestLabelMerge(t *testing.T) {
+	store := v1.Store{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "name",
+		},
+		Spec: v1.StoreSpec{
+			Container: v1.ContainerSpec{
+				Labels: map[string]string{
+					"test": "test",
+					"app":  "selector",
+				},
+			},
+		},
+	}
+
+	overwrite := make(map[string]string)
+	overwrite["test"] = "overwrite"
+	overwrite["test2"] = "test2"
+
+	labels := util.GetDefaultContainerStoreLabels(store, overwrite)
+	fmt.Println(labels)
+	require.Equal(t, "selector", labels["app"])
+	require.Equal(t, "overwrite", labels["test"])
+	require.Equal(t, "test2", labels["test2"])
+	require.Equal(t, "name", labels["shop.shopware.com/store.name"])
+}
