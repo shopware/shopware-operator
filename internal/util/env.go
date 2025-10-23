b428f653290ab28450f4f671a2e6caa2f0fe1405
Patrick Derks <patrick.derks@proton.me>
2025-10-23 17:03:35 +0200

    fix: env util

--- internal/util/env.go
+++ internal/util/env.go
@@ -11,12 +11,18 @@ import (
 func MergeEnv(dst, src []corev1.EnvVar) []corev1.EnvVar {
 	toAppend := []corev1.EnvVar{}
 	for si, srcEnv := range src {
+		found := false
 		for di, dstEnv := range dst {
 			// Overwrite if name exists in both slices
 			if srcEnv.Name == dstEnv.Name {
 				dst[di] = src[si]
+				found = true
 				break
 			}
+		}
+		// If not found in dst, add it to toAppend
+		if !found {
+			toAppend = append(toAppend, src[si])
 		}
 	}
 	return append(dst, toAppend...)
